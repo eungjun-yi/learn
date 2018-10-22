fe143428a09eb79ac269cb9a71b3b871c9d81521
Yi EungJun <eungjun.yi@gmail.com>
2018-10-22 14:26:58 +0900

    mono cache

--- src/test/kotlin/MonoTest.kt
+++ src/test/kotlin/MonoTest.kt
@@ -11,4 +11,20 @@ class MonoTest {
         }
         mono.block()
     }
+
+    @Test
+    fun repeat() {
+        val mono = Mono.fromCallable { System.out.println("publisher works"); 1 }
+        // map 할 때 마다 publisher가 실행된다.
+        mono.map { it * 2 }.block()
+        mono.map { it * 2 }.block()
+    }
+
+    @Test
+    fun cache() {
+        val mono = Mono.fromCallable { System.out.println("publisher works"); 1 }.cache()
+        // cache 하면 한번만 실행된다.
+        mono.map { it * 2 }.block()
+        mono.map { it * 2 }.block()
+    }
 }
